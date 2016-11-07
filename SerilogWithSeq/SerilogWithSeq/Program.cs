d267f11e7a684a394307d071e7ecc09288fd4151
Rafael Cruz <Rafael Cruz@DESKTOP-5EGP7JC>
2016-11-07 21:10:42 -0200

    Initial Commit

---
+++ SerilogWithSeq/SerilogWithSeq/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerilogWithSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            for (int i = 0; i < 1500; i++)
+            {
+                var request = new
+                {
+                    TransactionId = Guid.NewGuid(),
+                    Message = "OK",
+                    Response = "Usuário salvo com sucesso"
+                };
+
+                log.Information("New Request {TransactionId}: {@request}", request.TransactionId, request);
+            }
+            Console.Read();
+        }
+    }
+}
